56a35f40e84b483e9db4b099be265fb2a89b63a9
saverio87 <mattolisaverio@gmail.com>
2021-08-11 02:14:50 +0800

    Second commit

--- send_message.py
+++ send_message.py
@@ -5,7 +5,7 @@ import credentials
 import random
 from telethon import TelegramClient
 from telethon.tl.types import DocumentAttributeVideo
-from telethon.tl.types import InputPeerChat
+from telethon.tl.types import Chat
 
 
 # Remember to use your own values from my.telegram.org!
@@ -58,8 +58,8 @@ async def send_template_c(chat):
     else:
         print('send_template_b ad_3')
 
-templates = {'ad_vid_1': send_template_a,
-             'ad_vid_2': send_template_b, 'ad_img_3': send_template_c}
+templates = {'message_1': send_template_a,
+             'message_2': send_template_b, 'message_3': send_template_c}
 templatesList = list(templates)
 
 
@@ -69,21 +69,25 @@ async def main():
 
     chats = await client.get_dialogs()
     for chat in chats:
+        # We only want to send messages to groups
+        if isinstance(chat.entity, Chat):
 
-        templateKey = templatesList[random.randrange(0, len(templatesList))]
-        start_time = time.time()
-        print('- Sending', templateKey, 'to group id ', chat.id)
-        template = templates[templateKey]
+            templateKey = templatesList[random.randrange(
+                0, len(templatesList))]
+            start_time = time.time()
+            print('- Sending', templateKey, 'to group id ',
+                  chat.entity.id, " - ", chat.entity.title)
+            template = templates[templateKey]
 
-        try:
-            await template(chat)
-            print('- Sent success, sleep a while')
-            time.sleep(5)
-            end_time = round(time.time() - start_time, 2)
-            print('- Sent success in ' + str(end_time) + ' seconds')
-        except Exception as e:
-            print('- Sent failed, bummer', chat.id, "Error: ", e)
-            templateKey = 'error'
+            try:
+                await template(chat)
+                print('- Sent success, sleep a while')
+                time.sleep(5)
+                end_time = round(time.time() - start_time, 2)
+                print('- Sent success in ' + str(end_time) + ' seconds')
+            except Exception as e:
+                print('- Sent failed, bummer', chat.id, "Error: ", e)
+                templateKey = 'error'
 
 
 with client:
